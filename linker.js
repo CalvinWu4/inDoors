9f7a6ec1ffc5249ac8b22b4f4038daec65e72c9f
Brian Lam <iBriaan@gmail.com>
2015-01-06 16:34:52 -0500

    Get useragent, pass in element for callback

--- linker.js
+++ linker.js
@@ -9,25 +9,25 @@ var genIP = function() {
 }
 
 var randomInt = function () {
-	return Math.floor((Math.random() * 253) + 1);
+	return Math.floor((Math.random() * 2) + 128);
 }
 
 /* Grab the GlassDoor Data given the company name */
-var gdinfo = function (name) {
+var gdinfo = function (element, name) {
 	var xmlhttp = new XMLHttpRequest();
-	var url = "https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=" + partnerid + "&t.k=" + apikey + "&action=employers&userip=" + genIP() + "&useragent=Mozilla%2F4.0&q=" + name;
+	var url = "https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=" + partnerid + "&t.k=" + apikey + "&action=employers&userip=" + genIP() + "&useragent=" + navigator.userAgent + "&q=" + name;
 	xmlhttp.open("GET", url, true);
 	
 	xmlhttp.onreadystatechange = function() {
 		var data;
 		if (xmlhttp.status == 200) {
+			console.log(url);
 			/* GET Successful, parse data into JSON object */
 			var response = xmlhttp.responseText;
 			console.log(response);
-			return JSON.parse(response);		
 		} else {
-			console.log("Bad");
-			return null;
+			/* GET Unsuccessful */
+
 		}
 	};
 
@@ -39,10 +39,11 @@ $(".description").hover(function() {
 	var link = $(this).find("bdi").find("a");
 	var name = link.html();
 
+	var element = $(this);
 	if(typeof(name) !== "undefined") { 
 		/* If we're in this loop, this was a valid company name. 
 		Grab the company name and strip it of HTML tags */
 		var cleanname = name.replace("<b>","").replace("</b>","");
-		var info = gdinfo(cleanname);
+		var info = gdinfo(element, cleanname);
 	}
 });
