7713c703fe24d9e4de689513582e9cfd4ab73029
briantotheyanyan <sorakeybade@gmail.com>
2015-01-07 18:30:48 -0500

    Started implementing a refresh date for when companies should be updated. Because of lingering API issues, I have been unable to properly test if it works.

--- linker.js
+++ linker.js
@@ -5,13 +5,19 @@ a hover event onto company names that appear in search results
 /* Check if company is already in localstorage */
 var checkDatabase = function(name) {
     if(localStorage[name]) {
-		return true;
+	return true;
     }
     return false;
 }
 /* Saving things into local storage */
-var save = function(name,rating) {
-    localStorage[name] = rating;
+var save = function(addName,addRating, addMonth, addDay, addYear) {
+    var employer = {
+	rating:addRating,
+	month:addMonth,
+	day:addDay,
+	year:addYear
+    };
+    localStorage[name] = employer;
 }
 
 /* Load rating */
@@ -30,38 +36,66 @@ var randomInt = function () {
 
 /* Grab the GlassDoor Data given the company name */
 var gdinfo = function (element, name) {
-    if(checkDatabase(name)) {
-    	/* Database entry hit - No need to send new HTTP Request */
-		var rating = load(name);
-		element.find(".glassdoor-rating").html(rating);
+    var currentDate = new Date();
+    if(checkDatabase(name)){
+	if (currentDate.getFullYear() > load(name).year || currentDate.getMonth() > load(name).month || currentDate.getDate() - 7 >= load(name).day) {
+    	    /* Database entry hit - No need to send new HTTP Request */
+            var rating = load(name).rating;
+            element.find(".glassdoor-rating").html(rating);
+	}
+	else{
+	    /* Database entry miss - Send new HTTP Request to Glassdoor API for rating info */
+	    var xmlhttp = new XMLHttpRequest();
+	    var url = "https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=" + partnerid + "&t.k=" + apikey + "&action=employers&userip=" + genIP() + "&useragent=" + navigator.userAgent + "&q=" + name;
+	    xmlhttp.open("GET", url, true);
+	    
+	    xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.status == 200) {
+		    console.log(url);
+		    /* GET Successful, parse data into JSON object */
+		    var response = JSON.parse(xmlhttp.responseText || "null");
+		    if (response != null) {
+			if(response["success"] == true) {
+			    var rating = response["response"].employers[0].overallRating;
+			    element.find(".glassdoor-rating").html(rating);
+			    save(name,rating,currentDate.getMonth(),currentDate.getDate(),currentDate.getFullYear());
+			}
+		    }
+		} else {
+		    /* GET Unsuccessful */
+			}
+	    };
+	    
+	    xmlhttp.send();
+	}
     }
     else{
     	/* Database entry miss - Send new HTTP Request to Glassdoor API for rating info */
-		var xmlhttp = new XMLHttpRequest();
-		var url = "https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=" + partnerid + "&t.k=" + apikey + "&action=employers&userip=" + genIP() + "&useragent=" + navigator.userAgent + "&q=" + name;
-		xmlhttp.open("GET", url, true);
-		
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.status == 200) {
-				console.log(url);
-				/* GET Successful, parse data into JSON object */
-				var response = JSON.parse(xmlhttp.responseText || "null");
-				if (response != null) {
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=" + partnerid + "&t.k=" + apikey + "&action=employers&userip=" + genIP() + "&useragent=" + navigator.userAgent + "&q=" + name;
+	xmlhttp.open("GET", url, true);
+	
+	xmlhttp.onreadystatechange = function() {
+	    if (xmlhttp.status == 200) {
+		console.log(url);
+		/* GET Successful, parse data into JSON object */
+		var response = JSON.parse(xmlhttp.responseText || "null");
+		if (response != null) {
 					if(response["success"] == true) {
-						    var rating = response["response"].employers[0].overallRating;
-						    element.find(".glassdoor-rating").html(rating);
-						    save(name,rating);
-						}
+					    var rating = response["response"].employers[0].overallRating;
+					    element.find(".glassdoor-rating").html(rating);
+					    save(name,rating,currentDate.getMonth(),currentDate.getDate(),currentDate.getFullYear());
 					}
-			} else {
-				/* GET Unsuccessful */
-			}
-		};
-
-		xmlhttp.send();
+		}
+	    } else {
+		/* GET Unsuccessful */
+	    }
+	};
+	
+	xmlhttp.send();
     }
 }
-
+    
 /* Append a rating box to the end of each description element */
 $(".description bdi").each( function() {
 	$(this).append("<div class='glassdoor-label-wrapper'><div class='glassdoor-label'>Rating: <span class='glassdoor-rating'></span><div><a class='glassdoor-link' style='color:white' href='http://www.glassdoor.com/index.htm'>Powered by  <img style='margin-left:6px' src='http://www.glassdoor.com/static/img/api/glassdoor_logo_80.png' title='Job Search' /></a></div></div></div>");
