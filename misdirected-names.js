9eff9dd89206be82f750795f99cbac6ef2cc470b
Calvin Wu <calvinwu4@gmail.com>
2020-09-11 19:32:01 -0400

    Update misdirected-names.js

--- misdirected-names.js
+++ misdirected-names.js
@@ -1,6 +1,7 @@
 const misdirectArray = [
     {'Bloomberg LP': 'Bloomberg'},
     {'Verizon Media Platform': 'Verizon Media'},
-    {'JPMorgan Chase & Co.': 'J.P. Morgan'}
+    {'JPMorgan Chase & Co.': 'J.P. Morgan'},
+    {'Walt Disney Direct-to-Consumer & International': 'The Walt Disney Company'}
   ]
   
