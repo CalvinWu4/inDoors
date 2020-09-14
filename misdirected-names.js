10601421e57bd1e919a996c8be3ab607400442b9
Calvin Wu <calvinwu4@gmail.com>
2020-09-14 11:44:37 -0400

    Update misdirected-names.js

--- misdirected-names.js
+++ misdirected-names.js
@@ -2,6 +2,8 @@ const misdirectArray = [
     {'Bloomberg LP': 'Bloomberg'},
     {'Verizon Media Platform': 'Verizon Media'},
     {'JPMorgan Chase & Co.': 'J.P. Morgan'},
-    {'Walt Disney Direct-to-Consumer & International': 'The Walt Disney Company'}
+    {'Walt Disney Direct-to-Consumer & International': 'The Walt Disney Company'},
+    {'Cockroach Labs, the Makers of CockroachDB': 'Cockroach Labs'},
+    {'Binary Fountain™': 'Binary Fountain'}
   ]
   
