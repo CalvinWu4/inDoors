e63702895ff8b41a23ab47d6de12b9943822168b
Calvin Wu <calvinwu4@gmail.com>
2020-10-03 23:58:59 -0400

    Removed unneeded name

--- misdirected-names.js
+++ misdirected-names.js
@@ -1,6 +1,5 @@
 const misdirectArray = [
     {'Verizon Media Platform': 'Verizon Media'},
-    {'JPMorgan Chase & Co.': 'J.P. Morgan'},
     {'Walt Disney Direct-to-Consumer & International': 'The Walt Disney Company'},
     {'Hearst Magazines': 'Hearst'}
   ]
