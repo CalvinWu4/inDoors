86568bf07cbf28995a9860c4c6260114f80fa3d2
Calvin Wu <calvinwu4@gmail.com>
2020-11-14 10:33:27 -0500

    Changed proxy API hosting site

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function (name, sender, sendResponse) {
         var xhr = new XMLHttpRequest();
-        let url = `https://glassdoor-api-proxy.azurewebsites.net/api/gdinfo?company=${name}`;
+        let url = `https://glassdoor.calvinwu4.workers.dev/?company=${name}`;
         xhr.open("GET", url, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
