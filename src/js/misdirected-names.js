ca8b8435e468658149924292c4ad117d304b0d86
Calvin Wu <calvinwu4@gmail.com>
2020-12-01 18:22:58 -0500

    Update misdirected-names.js

--- src/js/misdirected-names.js
+++ src/js/misdirected-names.js
@@ -2,6 +2,7 @@ const misdirectArray = [
     {'Verizon Media Platform': 'Verizon Media'},
     {'Walt Disney Direct-to-Consumer & International': 'The Walt Disney Company'},
     {'Hearst Magazines': 'Hearst'},
-    {'Zappos Family of Companies': 'The Zappos Family'}
+    {'Zappos Family of Companies': 'The Zappos Family'},
+    {'Just Eat Takeaway.com': 'Just Eat'}
   ]
   
