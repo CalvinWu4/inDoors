5a77f491984a1a0aaf655faf8d39b9df4dfdbbd2
Calvin Wu <calvinwu4@gmail.com>
2020-12-09 03:16:23 -0500

    Got it partially working

--- src/js/indeed.js
+++ src/js/indeed.js
@@ -19,51 +19,31 @@ if(eventNode) {
     appendGlassdoor(eventNode.parentNode, eventNode.textContent);
 }
 
-// /viewjob
+// /viewjob left list
 const viewjobNode = document.querySelector('.jobsearch-InlineCompanyRating a');
 
 if(viewjobNode) {
     appendGlassdoor(viewjobNode.parentNode.parentNode, viewjobNode.textContent);
 }
 
-// // Get the iframe body
-// let iframe = document.getElementById('vjs-container-iframe').document.body;
-// Setup the config
-let config = { attributes: true, childList: true }
-// Create a callback
-let callback = function(mutations) {
-     /* callback actions */ 
-     for(let mutation of mutations) {
-		for(let node of mutation.addedNodes) {
-				if (!(node instanceof HTMLElement)) continue;	// we track only elements, skip other nodes (e.g. text nodes)
-
-				// check the inserted element
-				const nameNode = node.querySelector(".jobsearch-InlineCompanyRating");
-				if (nameNode) {
-					const name = nameNode.textContent;
-					appendGlassdoor(nameNode.parentNode, name, twoLines=false);
-				}
-			}
-		}
-    }
-
-// Watch the iframe for changes
-// let observer = new MutationObserver(callback)
-// observer.observe(iframe, config)
-
-
+// /viewjob right iframe
 new MutationObserver(function(mutations) {
 	for(let mutation of mutations) {
 		for(let node of mutation.addedNodes) {
-				if (!(node instanceof HTMLElement)) continue;	// we track only elements, skip other nodes (e.g. text nodes)
-
-				// check the inserted element
-                // Get the iframe body
-                let iframe = document.getElementById('vjs-container-iframe');
-				if (iframe) {
-                    let observer = new MutationObserver(callback)
-                    observer.observe(iframe.contentDocument, config)
-				}
-			}
+                if (node instanceof HTMLElement) {
+                    // Watch the iframe for changes
+                    let iframe = document.getElementById('vjs-container-iframe');
+                    if (iframe) {
+                        iframe.addEventListener("load", function() {
+                            iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<style> .glassdoor-rating { color: #2d2d2d !important; } </style>';
+                          
+                            const viewjobNode = iframe.contentDocument.querySelector('.jobsearch-InlineCompanyRating a');
+                            if (viewjobNode) {
+                                appendGlassdoor(viewjobNode.parentNode.parentNode, viewjobNode.textContent);
+                            }
+                        });
+                    }
+                }
+            }
 		}
 }).observe(document, {subtree: true, childList: true});
