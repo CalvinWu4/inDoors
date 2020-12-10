7bc651237c6a25c038892a1b1f98baa3c50b0580
Calvin Wu <calvinwu4@gmail.com>
2020-12-10 00:35:48 -0500

    Got rating on Indeed's iframe fully working

--- src/js/indeed.js
+++ src/js/indeed.js
@@ -26,6 +26,109 @@ if(viewjobNode) {
     appendGlassdoor(viewjobNode.parentNode.parentNode, viewjobNode.textContent);
 }
 
+const css =                             
+`<style> 
+/* 
+* === General styles ===
+*/
+.glassdoor-label-wrapper {
+    position: relative;
+}
+
+.glassdoor-label {
+font-weight: 400;
+color: #2d2d2d !important;
+}
+
+#glassdoor-link {
+position: relative;
+}
+
+#glassdoor-link, :visited {
+    font-weight: 400;
+    color: #2d2d2d !important;
+}
+
+a#glassdoor-link:hover {
+color: var(--color-action-active);
+text-decoration: underline;
+}
+
+.glassdoor-rating {
+color: #0caa41;
+font-weight: bold;
+}
+
+.display-none {
+display: none;
+}
+
+/* Remove Glassdoor logo link */
+a[href="https://www.glassdoor.com/index.htm"]{
+pointer-events: none;
+}
+
+/* 
+* === Columnate Glassdoor wrapper ===
+*/
+.cell {
+    display: table-cell;
+    float: none;
+}
+
+.middle {
+    vertical-align: middle;
+}
+
+.pad-right {
+    padding-right: 10px;
+}
+
+/* 
+* === Indeed.com styles ===
+*/
+.glassdoor-label-wrapper {
+  font-size: initial;
+}
+  
+.glassdoor-label {
+  color: #2d2d2d !important;
+}
+  
+.glassdoor-reviews {
+  color: #2d2d2d !important
+}
+  
+#glassdoor-link {
+  color: #2d2d2d !important;
+  text-decoration-color: #2d2d2d !important;
+}
+  
+/* /jobs */
+.jobsearch-SerpJobCard .logowrapper {
+  transform: translate(0, .15rem);
+}
+  
+/* /cmp/ */
+.cmp-CompactHeaderLayout-companyInfo .logowrapper {
+  transform: unset;
+}
+  
+/* /event */
+.event-page .logowrapper {
+  transform: translate(0, -.1rem);
+}
+  
+/* /viewjob */
+.jobsearch-ViewJobLayout-jobDisplay .logowrapper {
+  transform: translate(0, .15rem);
+}
+
+.jobsearch-JobComponent-embeddedHeader .logowrapper {
+  transform: translate(0, .15rem);
+}
+</style>`;
+
 // /viewjob right iframe
 new MutationObserver(function(mutations) {
 	for(let mutation of mutations) {
@@ -35,9 +138,10 @@ new MutationObserver(function(mutations) {
                     let iframe = document.getElementById('vjs-container-iframe');
                     if (iframe) {
                         iframe.addEventListener("load", function() {
-                            iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<style> .glassdoor-rating { color: #2d2d2d !important; } </style>';
+                            // Inject main document's CSS
+                            iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML.replace(css) + css;
                           
-                            const viewjobNode = iframe.contentDocument.querySelector('.jobsearch-InlineCompanyRating a');
+                            const viewjobNode = iframe.contentDocument.querySelector('.jobsearch-InlineCompanyRating > div:first-child');
                             if (viewjobNode) {
                                 appendGlassdoor(viewjobNode.parentNode.parentNode, viewjobNode.textContent);
                             }
