70c176ad5675ac9410af610cd5c095097c2dfd2b
Calvin Wu <calvinwu4@gmail.com>
2021-02-23 18:37:54 -0500

    Tweak API call to prevent occasional routing to wrong page

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function (name, sender, sendResponse) {
         var xhr = new XMLHttpRequest();
-        let url = `https://glassdoor.calvinwu4.workers.dev/?company=${name}`;
+        let url = `https://glassdoor.calvinwu4.workers.dev/?company=${name};`;
         xhr.open("GET", url, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
