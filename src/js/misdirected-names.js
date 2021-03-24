83499abda920fd0c4ba8aaa6228ef618e93cfad2
Calvin Wu <calvinwu4@gmail.com>
2021-03-24 18:28:35 -0400

    Update misdirected-names.js

--- src/js/misdirected-names.js
+++ src/js/misdirected-names.js
@@ -1,6 +1,7 @@
 const misdirectArray = [
     {'WB Games': 'Warner Bros.'},
     {'Just Eat Takeaway.com': 'Just Eat'},
-    {'X, the moonshot factory': 'X Development'}
+    {'X, the moonshot factory': 'X Development'},
+    {'Wise Ltd.': 'TransferWise'}
   ]
   
