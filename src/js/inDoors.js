6e7a7173049005cef6078aa7c203a83c7f76cf65
Calvin Wu <calvinwu4@gmail.com>
2024-01-19 17:33:18 -0800

    Updated documentation to reflect changes to proxy API

--- src/js/inDoors.js
+++ src/js/inDoors.js
@@ -99,7 +99,7 @@ async function addRating(element, name, originalName=null) {
 		//  Data schema wasn't changed
 		(JSON.stringify(Object.keys(storageData)) === JSON.stringify(returnDataKeys) 
 		|| JSON.stringify(Object.keys(storageData)) === JSON.stringify(['retrievalDate', 'expirationDate', 'url']))
-		// Entry was saved less than a week ago
+		// Entry is not expired
 		&& new Date() < new Date(Date.parse(storageData.expirationDate))) {
 		// Database entry hit - Use recent data from in localstorage.
 		updateRating(element, storageData);
